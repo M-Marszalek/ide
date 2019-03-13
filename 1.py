f6a5d07792e4a0c9595a9e72f85810048be3a38d
M-Marszalek <48250522+M-Marszalek@users.noreply.github.com>
2019-03-13 09:34:13 +0100

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 13 09:19:17 2019
+
+@author: User
+"""
+
+import numpy as np
+import matplotlib.pyplot as pl
+u = [3,2,1,-1]
+y = [7,3,2,1]
+z = []
+
+def q (teta):
+    k=0
+    for i in range(len(u)):
+        k = k+((y[i]-teta*u[i])**2)
+    return k
+
+r = np.linspace(-5,20,100)
+w = np.zeros(len(r))
+for p in range(len(r)):
+    w[p]= q(r[p])
+
+pl.plot(r,w)
+pl.show()
