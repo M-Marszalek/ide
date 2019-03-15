fa228ae4c4f678f7d4b8957c157fa0f69a0f9d7d
M-Marszalek <48250522+M-Marszalek@users.noreply.github.com>
2019-03-15 21:13:12 +0100

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,44 @@
+import numpy as np
+import matplotlib.pyplot as pl
+import random as rn
+
+u = [2,1,-1]
+y = [3,2,1]
+z = []
+
+def q (teta):
+    k=0
+    for i in range(len(u)):
+        k = k+((y[i]-teta*u[i])**2)
+    return k
+dl =100
+r = np.linspace(-20,20,dl)
+w = np.zeros(len(r))
+for p in range(len(r)):
+    w[p]= q(r[p])
+
+minima = []
+indexy =[]
+m = int(rn.randint(0,dl-1))
+mini = w[m]
+indexy.append(r[m])
+minima.append(mini)
+print(m)
+s=0
+
+while s<100 :
+    m = int(rn.randint(0,dl-1))
+    x = w[m]
+    print(m)
+    if x < mini:
+        mini = x
+        s=0
+        indexy.append(r[m])
+        minima.append(mini)
+    else:
+        s= s+1
+
+
+pl.plot(r,w,'r')
+pl.scatter(indexy,minima)
+pl.show()
---
+++ 3.py
@@ -0,0 +1,42 @@
+import numpy as np
+import matplotlib.pyplot as pl
+
+
+u = [2,1,-1]
+y = [3,2,1]
+z = []
+
+def q (teta):
+    k=0
+    for i in range(len(u)):
+        k = k+((y[i]-teta*u[i])**2)
+    return k
+a=0
+b=2001
+dl = b-a
+r = np.linspace(-71,27,dl)
+w = np.zeros(len(r))
+for p in range(len(r)):
+    w[p]= q(r[p])
+    
+pol = int(dl/2)
+polowy=[]
+pola=[]
+
+while dl>1 :
+    pol= int(dl/2)+a
+    p1=w[pol-1]
+    p2=w[pol+1]
+    if p1 < p2:
+        b = int(pol)
+    else:
+        a = int(pol)
+    dl = b-a
+    print(dl)
+    polowy.append(w[pol])
+    pola.append(r[pol])
+    
+    
+pl.plot(r,w,'r')
+pl.scatter(pola,polowy)
+pl.show()
